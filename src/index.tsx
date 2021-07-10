6b8d1f39fc48454df0121b9214b130ca0b6403cd
rodrigo <digorinaldi@gmail.com>
2021-07-10 13:43:40 -0300

    fet createTransaction, mirageTransactons

--- src/index.tsx
+++ src/index.tsx
@@ -1,25 +1,48 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { createServer } from 'miragejs'
+import { createServer, Model } from 'miragejs'
 import { App } from './App';
 
 createServer({
+  models: {
+    transaction: Model,
+  },
+
+  seeds(server) {
+    server.db.loadData({
+      transactions: [
+        {
+          id: 1,
+          title: 'Freelancer de web site',
+          type: 'deposit',
+          category: 'Dev',
+          amount: 750,
+          createdAt: new Date('2021-02-12 9:00:00'),
+        },
+        {
+          id: 2,
+          title: 'Aluguel',
+          type: 'withdraw',
+          category: 'Casa',
+          amount: 1500,
+          createdAt: new Date('2021-05-12 12:00:00'),
+        }
+      ],
+    })
+  },
+
   routes() {
     this.namespace = 'api';
 
     this.get('/transactions', () => {
-      return  [
-        {
-          id: 1,
-          title: 'Transaction 1',
-          amount: 400,
-          type: 'deposit',
-          category: 'Food',
-          creatAt: new Date()
-        }
-      ]
+      return  this.schema.all('transaction')
     })
 
+    this.post('/transactions', (schema, request) => {
+      const data = JSON.parse(request.requestBody);
+
+      return schema.create('transaction', data);
+    })
   }
 })
 
